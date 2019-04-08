76657325a9635c3be72b831d03113240c117005c
Eric Bottard <ebottard@pivotal.io>
2019-04-08 16:26:06 +0200

    Fix reduce

--- src/main/java/com/example/kmprssr/Kmprssr.java
+++ src/main/java/com/example/kmprssr/Kmprssr.java
@@ -1,5 +1,6 @@
 package com.example.kmprssr;
 
+import java.io.IOException;
 import java.util.function.Function;
 
 import reactor.core.publisher.Flux;
@@ -13,7 +14,12 @@ public class Kmprssr implements Function<Flux<String>, Flux<String>> {
 		i++;
 		return input
 				.window(3)
-				.concatMap(ss -> Flux.just("Invocation " + i + " " + ss.map(s -> s.replaceAll("[aeiouâãäåæèéêëıìíîïðñòóôõøùúûü]*", "")).reduce((a, b) -> a + " " + b)));
+				.concatMap(ss -> ss.map(s -> s.replaceAll("[aeiouâãäåæèéêëıìíîïðñòóôõøùúûü]*", "")).reduce("Invocation " + i + "", (a, b) -> a + " " + b));
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Kmprssr().apply(Flux.just("riff", "is", "for", "functions")).subscribe(System.out::println);
+		System.in.read();
 	}
 
 }
